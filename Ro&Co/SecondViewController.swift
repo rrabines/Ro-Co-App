facc01118bec2852f7c05e916cb28327635101fb
Rosana <xoxorosana@gmail.com>
2018-02-10 15:18:42 -0600

    Initial Commit

---
+++ Ro&Co/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  Ro&Co
+//
+//  Created by Rosana Rabines on 2/10/18.
+//  Copyright © 2018 Xoxo, Rosana. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
